af674ff6095bb70ba1529365b5c80d17b785fdd6
Samll-Kosmos <arturo.horcasitas@kosmos.la>
2020-07-23 22:25:34 -0500

    Use python file as config file

---
+++ ImageNet/config.py
@@ -0,0 +1,7 @@
+# Datasets path
+train_path = r'E:\ILSVRC\Data\CLS-LOC\train'
+dev_path = r'E:\ILSVRC\Data\CLS-LOC\dev'
+test_path = r'E:\ILSVRC\Data\CLS-LOC\dev'
+
+# Code index class map file
+names = r'C:\Users\artur\Desktop\artur\blog\ImageNet\code_class_map.txt'
