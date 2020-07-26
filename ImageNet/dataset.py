ab8ab43ced5fcca2a69a87af2115c8a72a05337f
Samll-Kosmos <arturo.horcasitas@kosmos.la>
2020-07-26 11:31:57 -0500

    Fix dataset

--- ImageNet/dataset.py
+++ ImageNet/dataset.py
@@ -1,6 +1,14 @@
-# import yaml
+from os.path import join as join_path
+from typing import Optional, Tuple, List, Callable
+from collections import namedtuple
+
+from PIL import Image
+from numpy import ndarray
+
 from torch.utils.data import Dataset
 from torchvision.datasets import ImageFolder
+
+from ImageNet import config
 
 
 class DatasetFactory:
@@ -8,12 +16,51 @@ class DatasetFactory:
     @staticmethod
     def get_dataset(split: str) -> Dataset:
         if split == 'train':
-            return ImageFolder(root=)
+            return ImageFolder()
         elif split == 'val':
             pass
         else:
             raise ValueError()
 
 
-class ValidationtDataset:
-    pass
+def read_data(kind: str) -> List[namedtuple]:
+    records = []
+    fpath = config.train_data if kind == 'train' else config.dev_data
+    Record = namedtuple('Record', ['fname', 'id', 'label', 'class_name'])
+    with open(fpath, 'rt') as f:
+        for line in f:
+            line = line.strip('\n').split()
+            r = Record(fname=line[0],
+                       id=line[1],
+                       label=line[2],
+                       class_name=line[3])
+            records.append(r)
+    return records
+
+
+class ImageNetDataset(Dataset):
+    """Load images from ImageNet dataset.
+
+    :param root: Directory containing the dataset.
+    :param kind: One of {train, dev}. Defines which dataset to use.
+    """
+    def __init__(self,
+                 kind: str,
+                 transform: Optional[Callable] = None) -> None:
+        self.transform = transform
+        self.img_paths, self.labels = [], []
+        for record in read_data(kind):
+            self.img_paths.append(join_path(config.train_path, record.fname))
+            self.labels.append(record.label)
+
+    def __len__(self):
+        return len(self.img_paths)
+
+    def __getitem__(self, index: int) -> Tuple[ndarray, ndarray]:
+        img = Image.open(self.img_paths[index])
+        label = self.labels[index]
+
+        if self.transform is not None:
+            img, label = self.transform(img, label)
+
+        return img, label
