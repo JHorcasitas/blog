5532af2408e358172bfeab262a7b79488d67ace5
Samll-Kosmos <arturo.horcasitas@kosmos.la>
2020-07-29 19:55:57 -0500

    Add logging

---
+++ ImageNet/__init__.py
@@ -0,0 +1,7 @@
+import logging
+
+
+logging.basicConfig(filename='training.log',
+                    filemode='w',
+                    format='[%(levelname)s] %(message)s',
+                    level=logging.INFO)
