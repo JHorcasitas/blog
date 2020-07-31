8ed6329c33c8e165ee8b0456b2d4d301d40c5b7f
Samll-Kosmos <arturo.horcasitas@kosmos.la>
2020-07-30 21:02:42 -0500

    Minor improvements

--- ImageNet/config.py
+++ ImageNet/config.py
@@ -9,10 +9,11 @@ train_path = r'E:\ILSVRC\Data\CLS-LOC\train'
 dev_path = r'E:\ILSVRC\Data\CLS-LOC\dev'
 
 # Annotations
-train_data = r'annotations\train_labels.txt'
-dev_data = r'annotations\dev_labels.txt'
+map_data_path = r'annotations\data_mapping.txt'
+train_data_path = r'annotations\train_labels.txt'
+dev_data_path = r'annotations\dev_labels.txt'
 
-
+# ImageNet statistics
 mean = np.array([0.485, 0.456, 0.406])
 std = np.array([0.229, 0.224, 0.225])
 
