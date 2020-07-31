cf2e866e64d66b5fd555b6bccdb991eec2b751eb
Samll-Kosmos <arturo.horcasitas@kosmos.la>
2020-07-30 21:02:50 -0500

    Fix metric tracker

--- ImageNet/metric_tracker.py
+++ ImageNet/metric_tracker.py
@@ -1,29 +1,33 @@
-from copy import deepcopy
-from statistics import mean
-from functools import partial
-from typing import Dict, Optional, List, Callable, Any
+import logging
+from typing import Dict
+from collections import defaultdict
 
 from torch import Tensor
 
+from ImageNet.config import map_data_path
+
 
 class MetricTracker:
-    """Keep track of TP, TN, FP, FN and compute some aggregation functions on
-    them.
-
-    :param class_names: Dictionary mapping the index of a class and its name.
-    """
+    """Keep track of TP, FP, FN and compute some aggregation functions on
+    them."""
 
     def __init__(self, class_names: Dict[int, str]) -> None:
-        self.num_classes = len(class_names)
-        self.class_names = class_names
+        index_list, class_list = [], []
+        with open(map_data_path, 'rt') as f:
+            for line in f:
+                line = line.strip('\n').split()
+                index_list.append(line[1])
+                class_list.append(line[2])
+        self.index_class_map = dict(zip(index_list, class_list))
+        self.num_classes = len(self.index_class_ma)
         self.reset()
 
     @property
     def precision(self):
         precision = {}
-        for k, v in self.index2name(self.metrics):
+        for k, v in self.metrics:
             try:
-                precision[k] = v['TP'] / (v['TP'] + v['TN'])
+                precision[k] = v['TP'] / (v['TP'] + v['FP'])
             except ZeroDivisionError:
                 precision[k] = None
         return precision
@@ -31,7 +35,7 @@ class MetricTracker:
     @property
     def recall(self):
         precision = {}
-        for k, v in self.index2name(self.metrics):
+        for k, v in self.metrics:
             try:
                 precision[k] = v['TP'] / (v['TP'] + v['FN'])
             except ZeroDivisionError:
@@ -39,32 +43,21 @@ class MetricTracker:
         return precision
 
     @property
-    def f1_score(self):
-        f1, p, r = {}, self.precision, self.recall
-        for name, in self.class_names:
-            try:
-                f1[name] = 2 * ((p[name] * r[name]) / (p[name] + r[name]))
-            except (ZeroDivisionError, TypeError):
-                f1[name] = None
-        return f1
-
-    @property
-    def global_f1_score(self):
-        f1 = self.f1_score
-        if all(v is not None for v in f1.values()):
-            return mean(f1.values())
-        else:
-            return None
+    def top_1_score(self):
+        resume = defaultdict(int)
+        for v in self.metrics.values():
+            resume['TP'] += v['TP']
+            resume['FP'] += v['FP']
+            resume['FN'] += v['FN']
+        return resume['TP'] / (resume['TP'] + resume['FP'] + resume['FN'])
 
     def reset(self):
-        """Deletes all previous saved data. Sets self.metrics to its default
-        value"""
+        """Deletes all previous saved data. Set self.metrics to its default
+        value. Metrics doesn't contain TN because it doesn't make sense for
+        a multi class classification problem."""
         self.metrics = {}
-        for i in range(1, self.num_classes + 1):
-            self.metrics[i] = {'TP': 0, 'TN': 0, 'FP': 0, 'FN': 0}
-
-    def increase_dict_count(self, dict_obj, key):
-        dict_obj[key] += 1
+        for i in range(0, self.num_classes):
+            self.metrics[i] = {'TP': 0, 'FP': 0, 'FN': 0}
 
     def update_metrics(self, pred: Tensor, target: Tensor) -> None:
         """
@@ -73,46 +66,14 @@ class MetricTracker:
         """
         for i in range(len(pred)):
             if target[i] == pred[i]:
-                self.nested_dict_update(
-                    dict_obj=self.metrics,
-                    keys=[target[i]],
-                    func=partial(self.increase_dict_count, key='TP'))
-                self.nested_dict_update(
-                    dict_obj=self.metrics,
-                    keys=[j for j in range(self.num_classes) if j != target[i]],
-                    func=partial(self.increase_dict_count, key='TN'))
+                self.metrics[target[i]]['TP'] += 1
             else:
-                self.nested_dict_update(
-                    dict_obj=self.metrics,
-                    keys=[target[i]],
-                    func=partial(self.increase_dict_count, key='FP'))
-                self.nested_dict_update(
-                    dict_obj=self.metrics,
-                    keys=[pred[i]],
-                    func=partial(self.increase_dict_count, key='FN'))
-                self.nested_dict_update(
-                    dict_obj=self.metrics,
-                    keys=[j for j in range(len(pred)) if j not in {target[i], pred[i]}],
-                    func=partial(self.increase_dict_count, key='TN'))
+                self.metrics[pred[i]]['FP'] += 1
+                self.metrics[target[i]]['FN'] += 1
 
-    def nested_dict_update(self,
-                           dict_obj: Dict[str, int],
-                           keys: List[int],
-                           func: Callable[[Dict[str, int]], None]) -> None:
-        for k, v in deepcopy(dict_obj.items()):
-            if k in keys:
-                func(dict_obj[k])
-
-    def index2name(self, dict_obj: Dict[int, Any]) -> Dict[str, Any]:
-        """Maps the keys of a dictionary from index values to class names."""
-        ret_dict = {}
-        for index, value in dict_obj.items():
-            ret_dict[self.class_names[index]] = value
-        return ret_dict
-
-    def log_metrics(self, logger, opt_msg: Optional[str] = None):
-        p, r, f = self.precision, self.recall, self.f1_score
-        for name in self.class_names:
-            base_msg = f'Class: {name}, Precision: {p[name]}, Recall: {r[name]}, F1 Score: {f[name]}'
-            full_msg = base_msg + ', ' + opt_msg if opt_msg else base_msg
-            logger.info(full_msg)
+    def log_metrics(self):
+        t1, p, r = self.top_1_score, self.precision, self.recall
+        for i in range(0, self.num_classes):
+            message = 'Class: {}, Precision: {}, Recall: {}, F1: {}'
+            class_name = self.index_class_map[i]
+            logging.info(message.format(class_name, p[i], r[i], t1[i]))
